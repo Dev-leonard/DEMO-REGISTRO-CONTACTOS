46335ae0f9d5273a91e93584c916cd4269fdd993
Leon Matias <leon.matias.dev@gmail.com>
2021-12-16 15:41:43 -0500

    segundo commit

--- main/java/com/agenda/contactos/controlador/ContactoControlador.java
+++ main/java/com/agenda/contactos/controlador/ContactoControlador.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -41,6 +42,39 @@ public class ContactoControlador {
         }
         contactoRepositorio.save(contacto);
         redirect.addFlashAttribute("msgExito", "El contacto ha sido agregado con exito");
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/editar")
+    public String mostrarFormularioDeEditarContacto(@PathVariable Integer id, Model modelo) {
+        Contacto contacto = contactoRepositorio.getById(id);
+        modelo.addAttribute("contacto", contacto);
+        return "nuevo";
+    }
+
+    @PostMapping("/{id}/editar")
+    public String actualizarContacto(@PathVariable Integer id, @Validated Contacto contacto, BindingResult bindingResult, RedirectAttributes redirect, Model modelo) {
+        Contacto contactoDB = contactoRepositorio.getById(id);
+        if (bindingResult.hasErrors()) {
+            modelo.addAttribute("contacto", contacto);
+            return "nuevo";
+        }
+
+        contactoDB.setNombre(contacto.getNombre());
+        contactoDB.setCelular(contacto.getNombre());
+        contactoDB.setEmail(contacto.getNombre());
+        contactoDB.setFechaNacimiento(contacto.getFechaNacimiento());
+
+        contactoRepositorio.save(contactoDB);
+        redirect.addFlashAttribute("msgExito", "El contacto ha sido actualizado correctamente");
+        return "redirect:/";
+    }
+
+    @PostMapping("/{id}/eliminar")
+    public String eliminarContacto(@PathVariable Integer id, RedirectAttributes redirect) {
+        Contacto contacto = contactoRepositorio.getById(id);
+       contactoRepositorio.delete(contacto);
+       redirect.addFlashAttribute("msgExito", "El contacto ha sido eliminado correctamente");
         return "redirect:/";
     }
 }
